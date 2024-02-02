b087e2331b920de5d743c17547bc9822c72bd0a1
Matyas2004 <76487285+Matyas2004@users.noreply.github.com>
2024-02-02 12:36:57 +0100

    Create listOfIds.ts

---
+++ listOfIds.ts
@@ -0,0 +1,95 @@
+// Text channels' keys are the exact discord names, doesn't include Sv. Jiří gamebook
+const TEXT_CHANNELS = {
+  pravidla: '689461158406783063',
+  schůzky: '689481714770116640',
+  oznámení: '875105873960247326',
+  'fotky-na-disku': '886717376928817244',
+  hráči: '689454260848558092',
+  generál: '689451644026486824',
+  memes: '689461602059157542',
+  'off-topič': '820972926739808286',
+  'hudební-krabice': '922166780724658256',
+  vedení: '689891170897362974',
+  'vedení-oddílu': '755096325241962667',
+  'rádcovská-družina': '1163508236582912070',
+  'memes-vedoucí': '962991487283503104',
+  fotky: '819145256427192340',
+  kurzy: '1073625095936946216',
+  'roverská-čajka': '771082407230963744',
+  'alt-roveři': '1024717458063499394',
+  'jung-roveři': '1024714194584612975',
+  'chat-medvědi': '689461844741587044',
+  'medvědi-vedení': '690978466937569360',
+  'chat-lišáci': '689461902123991044',
+  'lišáci-vedení': '887025418794835969',
+  'chat-káňata': '689461983640027143',
+  'káňata-vedení': '955146082977992765',
+  'chat-netopýři': '875106310721527859',
+  'netopýři-vedení': '952967532216741888',
+  přihlášení: '689474394250018951',
+};
+
+// Voice channels' keys are the exact discord names inclunding Capitalization and spaces
+const VOICE_CHANNELS = {
+  Schůzky: '689451644026486839',
+  'Povídání si': '836293269641363516',
+  Hráči: '689454353089822752',
+  Vedení: '689486777203163186',
+  'Vedení oddílu': '755096426777935974',
+  'Vedení hráči': '755098048618823862',
+  'Roverský potlach': '771082502348472330',
+  'Roverští hráči': '771082568274804756',
+  'Hovor medvědi': '689461878522773529',
+  'Hovor lišáci': '689461936106373193',
+  'Hovor káňata': '689462021749604412',
+  'Káňata vedení': '1148325639213035660',
+  'Hovor netopýři': '875106350814867567',
+};
+
+// Roles' keys are the exact discord names inclunding Capitalization and spaces, doesn't include bot roles
+const ROLES = {
+  'Vedoucí měsíce': '1074435047563743345',
+  'Vůdce oddílu': '1013172823600148571',
+  Admin: '689452676257546343',
+  Ztlumen: '695160007490142239',
+  'Oddíloví rádci': '755095927869407332',
+  'Rádci skautek': '836288116109213707',
+  Rádci: '689462505545924655',
+  Oldskauti: '1008084473801351198',
+  Roveři: '771081620945371146',
+  'Hlavní mašinfýra': '866332797156851744',
+  Skautky: '836287938076737606',
+  Medvědi: '689469952188481586',
+  Lišáci: '689470216052408353',
+  Káňata: '689470397359718473',
+  Netopýři: '874598796426878998',
+  Skauti: '836288373903720458',
+  Vedení: '704008567476191232',
+  Vlčci: '1063052384491798528',
+  Patroni: '1090559342798970900',
+};
+
+// Categories' keys are the exact discord names in ALL CAPS
+const CATEGORIES = {
+  DŮLEŽITÉ: '820971371114790942',
+  'TEXTOVÉ KANÁLY': '689451644026486802',
+  'HLASOVÉ KANÁLY': '689451644026486808',
+  VEDOUCÍ: '1073624378471878666',
+  ROVEŘI: '771082323654606929',
+  MEDVĚDI: '689461615762210817',
+  LIŠÁCI: '689461776223699121',
+  KÁŇATA: '689461812932116517',
+  NETOPÝŘI: '875106250264834120',
+  NOVÁČCI: '689474103379623990',
+  'SV. JIŘÍ GAMEBOOK #1': '836206476912623646',
+  'SV. JIŘÍ GAMEBOOK #2': '702164039521796118',
+  'SV. JIŘÍ GAMEBOOK #3': '836205998137147402',
+};
+
+const GUILD_ID = '689451643468775550';
+
+export { TEXT_CHANNELS };
+export { VOICE_CHANNELS };
+export { ROLES };
+export { CATEGORIES };
+export { GUILD_ID };
